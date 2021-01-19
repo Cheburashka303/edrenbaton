c26b70d3faa7484f82c72cfc5715f470b8d3a724
AlmiriQ <50090571+AlmiriQ@users.noreply.github.com>
2021-01-19 16:55:53 +1000

    Update yoperniiteatr.py

--- yoperniiteatr.py
+++ yoperniiteatr.py
@@ -2,9 +2,5 @@
 
 Функция декодирования называется decode_from_morse(code)."""
 
-def encode_to_morse(text: str) -> str:
-    pass
-
-
-def decode_from_morse(code: str) -> str:
-    pass
+from encode import encode_to_morse
+from decode import decode_from_morse
