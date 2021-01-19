2b1c45ff25b9f7e39a6d0c2181ff4774506e231c
AlmiriQ <50090571+AlmiriQ@users.noreply.github.com>
2021-01-19 17:01:12 +1000

    Update decode.py
    idk if it works

--- decode.py
+++ decode.py
@@ -1,1 +1,10 @@
-
+def decode_from_morse(code: str) -> str:
+	global MorseCode
+	buffer = ""
+	result = ""
+	for char in code:
+		buffer += code
+		if buffer in MorseCode:
+			result += MorseCode[buffer]
+			buffer = ""
+	return result
