5eb6ad86c5d2a9ce9050edab565224c00922cc98
Cheburashka303 <77660365+Cheburashka303@users.noreply.github.com>
2021-01-19 17:09:55 +1000

    Update encode.py
    Meine мнение = ёлки иголки

--- encode.py
+++ encode.py
@@ -1,1 +1,12 @@
+from dicktionary import MorseCode
 
+
+def encode_to_morse(text: str) -> str:
+	global MorseCode
+	result = ''
+	for char in text:
+		if char in MorseCode:
+			result += MorseCode[char]
+		else:
+			result += char
+	return result
